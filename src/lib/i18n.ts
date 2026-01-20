cbc75b37fc0d9a6e63a7965071e774db28984f3b
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2026-01-20 13:33:57 +0100

    add

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,12 +1,23 @@
-// src/lib/i18n.ts
-import { addMessages, init } from 'svelte-i18n';
+import { addMessages, init, locale } from 'svelte-i18n';
+import { browser } from '$app/environment';
 import en from './locales/en.json';
 import hu from './locales/hu.json';
 
 addMessages('en', en);
 addMessages('hu', hu);
 
+const savedLocale = browser ? window.localStorage.getItem('locale') : null;
+const initialLocale = savedLocale || 'hu';
+
 init({
   fallbackLocale: 'en',
-  initialLocale: 'hu',
+  initialLocale: initialLocale,
 });
+
+if (browser) {
+  locale.subscribe((value) => {
+    if (value) {
+      window.localStorage.setItem('locale', value);
+    }
+  });
+}
